30599403dafbcb12a6958d1591f533c2974d549e
osulca <omarsulca@gmail.com>
2020-06-17 09:34:32 -0500

    validacion formularios

--- reg_usuario.php
+++ reg_usuario.php
@@ -4,8 +4,28 @@ use Clases\Usuario as Usuario;
 use Clases\Estudiante as Estudiante;
 
 include_once "config/autoload.php";
+
+function validarUsuario($usuario): bool
+{
+    return (filter_var($usuario, FILTER_SANITIZE_STRING) === False) ? false : true;
+}
+
+function validarUsuario2($usuario): bool
+{
+    return preg_match("/[a-zA-Z]+/", $usuario);
+}
+
+function validarPassword($password): bool
+{
+    $length = strlen($password);
+    if ($length < 8) {
+        return false;
+    }
+    return true;
+}
+
 ?>
-    // formulario de registro que muestre los estudiantes en select para relacionar
+    <h1>Registro de Usuarios</h1>
     <form method="post" action="<?= $_SERVER["PHP_SELF"] ?>">
         <input type="text" name="usuario" placeholder="Ingrese nuevo usuario"/><br>
         <input type="password" name="pass" placeholder="Ingrese contraseña"/><br>
@@ -19,7 +39,7 @@ include_once "config/autoload.php";
             $estudiante = new Estudiante();
             $resultados = $estudiante->getDataEstudiantes();
             foreach ($resultados as $item) {
-                echo "<option value='" . $item["id"] . "'>" . $item["nombres"]." ".$item["apellidos"] . "</option>";
+                echo "<option value='" . $item["id"] . "'>" . $item["nombres"] . " " . $item["apellidos"] . "</option>";
             }
             ?>
         </select><br>
@@ -27,24 +47,43 @@ include_once "config/autoload.php";
     </form>
 <?php
 if (isset($_POST["submit"])) {
-    $usuario = $_POST["usuario"];
-    $password = $_POST["pass"];
+    $usuario = strtolower(trim($_POST["usuario"]));
+    $password = strtolower(trim($_POST["pass"]));
     $tipo = $_POST["tipo"];
     $id_estudiante = $_POST["id_estudiante"];
 
-    $password = password_hash($password, PASSWORD_BCRYPT);
+    $errores = [];
 
-    $user = new Usuario();
-    $user->setUsuario($usuario);
-    $user->setPassword($password);
-    $user->setTipo($tipo);
-    $user->setIdEstudiante($id_estudiante);
-
-    $result = $user->crearUsuario();
-    if ($result == true) {
-        echo "usuario creado";
-    } else {
-        echo "Error: usuario no creado";
+    if ((strlen($usuario) == 0) or (strlen($password) == 0)) {
+        $errores[] = "complete los campos";
     }
 
+    if (validarUsuario2($usuario) != true) {
+        $errores[] = "el usuario no tiene el formato adecuado";
+    }
+
+    if (validarPassword($password) != true) {
+        $errores[] = "la contraseña debe tener al menos 8 caracteres";
+    }
+
+    foreach ($errores as $error) {
+        echo $error . "<br>";
+    }
+
+    if (count($errores) == 0) {
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        $user = new Usuario();
+        $user->setUsuario($usuario);
+        $user->setPassword($password);
+        $user->setTipo($tipo);
+        $user->setIdEstudiante($id_estudiante);
+
+        $result = $user->crearUsuario();
+        if ($result == true) {
+            echo "usuario creado";
+        } else {
+            echo "Error: usuario no creado";
+        }
+    }
 }
